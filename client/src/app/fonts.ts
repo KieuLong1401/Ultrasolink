b1911c574ae6ee6ae705980504eb7e69291f9253
Kieu Long <kieulong1401@gmail.com>
2024-09-26 11:12:34 +0000

    setup frontend

---
+++ client/src/app/fonts.ts
@@ -0,0 +1,13 @@
+import { Lato, Cormorant_Garamond } from "next/font/google";
+
+export const lato = Lato({
+    subsets: ['latin'],
+    style: 'normal',
+    weight: ["100", "300", "400", "700", "900"]
+})
+
+export const cormorant_Garamond = Cormorant_Garamond({
+    subsets: ['latin'],
+    style: 'normal',
+    weight: ["300", "400", "700", "500", "600"]
+})
