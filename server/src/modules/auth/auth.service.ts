9b81f95c57fc8794a9b907838131d2f53c79ef1c
KieuLong <kieulong1401@gmail.com>
2025-03-27 11:01:31 +0900

    add id to token payload

--- server/src/modules/auth/auth.service.ts
+++ server/src/modules/auth/auth.service.ts
@@ -28,7 +28,7 @@ export class AuthService {
     }
 
     async login(user: any): Promise<{ access_token: string }> {
-        const payload = { email: user.email } //
+        const payload = { email: user.email, id: user._id.toString() }
         return {
             access_token: this.jwtService.sign(payload),
         }
--- server/src/types/AccessTokenPayload.ts
+++ server/src/types/AccessTokenPayload.ts
@@ -1,3 +1,4 @@
 export type AccessTokenPayload = {
     email: string
+    id: string
 }
